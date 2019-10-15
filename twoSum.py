255144930a95370e6658b6b40645beb47ac405cf
shamisolchikamhi <shamisolchikamhi@gmail.com>
2019-10-15 12:07:04 +0200

    twoSum Solution faster than 64%

--- twoSum.py
+++ twoSum.py
@@ -1,24 +1,12 @@
 
 class Solution:
-    def twoSum(self, nums, target):
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         i = 0
-        j = 1
         x = len(nums)
         for m  in range(i,x,1):
              for k in range(m+1,x,1):
                  l = nums[m]+ nums[k]
                  if l == target:
-                     print([m,k])
-
-
-        
-
-def main():
-    List = [2,5,5,11]
-    d = Solution()
-    d.twoSum(List,10)
-
-if __name__ == "__main__":
-    main()
+                        return [m,k]        
 
 
